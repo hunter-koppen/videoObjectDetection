88cef12f8ab86a9fae1184173f39864933fe9c58
Hunter Koppen <hunter.koppen@kobeon.nl>
2025-03-12 21:23:01 +0100

    added screenshot functionality

--- src/CameraStream.jsx
+++ src/CameraStream.jsx
@@ -4,6 +4,16 @@ import { Camera } from "./components/camera";
 import "./ui/CameraStream.css";
 
 export function CameraStream(props) {
+    const handleScreenshotTaken = base64String => {
+        const cleanBase64 = base64String.replace(/^data:image\/jpeg;base64,/, "");
+        if (props.screenshotBase64String) {
+            props.screenshotBase64String.setValue(cleanBase64);
+            if (props.onScreenshotCapture && props.onScreenshotCapture.canExecute) {
+                props.onScreenshotCapture.execute();
+            }
+        }
+    };
+
     return (
         <Camera
             content={props.content}
@@ -11,6 +21,9 @@ export function CameraStream(props) {
             width={props.width.value}
             height={props.height.value}
             alignment={props.alignment}
+            takeScreenshot={props.takeScreenshot}
+            onScreenshot={handleScreenshotTaken}
+            screenshotBase64String={props.screenshotBase64String}
         />
     );
 }
--- src/components/camera.jsx
+++ src/components/camera.jsx
@@ -1,15 +1,24 @@
-import { createElement } from "react";
+import { createElement, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 
 export function Camera(props) {
     const alignment = props.alignment || "top";
-    
+    const webcamRef = useRef(null);
+
+    useEffect(() => {
+        if (props.takeScreenshot.value === true && webcamRef.current) {
+            const screenshot = webcamRef.current.getScreenshot();
+            if (props.onScreenshot && screenshot) {
+                props.takeScreenshot.setValue(false);
+                props.onScreenshot(screenshot);
+            }
+        }
+    }, [props.takeScreenshot, props.onScreenshot]);
+
     return (
         <div className={"mx-camerastream " + props.classNames}>
-            <Webcam height={props.height} width={props.width} screenshotFormat="image/jpeg" />
-            <div className={`camera-content-overlay camera-align-${alignment}`}>
-                {props.content}
-            </div>
+            <Webcam ref={webcamRef} height={props.height} width={props.width} screenshotFormat="image/jpeg" />
+            <div className={`camera-content-overlay camera-align-${alignment}`}>{props.content}</div>
         </div>
     );
 }
